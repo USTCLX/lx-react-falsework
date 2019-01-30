4ed47abd8a782336483b10791abc428dc243810c
lixiang <348816986@qq.com>
2019-01-31 00:00:19 +0800

    update - @babel/plugin-proposal-class-properties

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@
  * @Author: lixiang 
  * @Date: 2019-01-30 14:34:25 
  * @Last Modified by: lixiang
- * @Last Modified time: 2019-01-30 14:37:07
+ * @Last Modified time: 2019-01-30 23:59:25
  */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+    state = {
+        count: 0
+    }
+
+    onClick = () => {
+        console.log('keke');
+        this.setState({
+            count: ++this.state.count
+        })
+    }
+
     render() {
-        return <h1>Hello World</h1>
+        return <div>
+            <h1>Hello World {this.state.count}</h1>
+            <button onClick={this.onClick}>click</button>
+        </div>
     }
 }
 
