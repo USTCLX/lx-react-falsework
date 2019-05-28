eb46ff9cecebdeb91226a600905b4755a93d7e52
lx <sa514075@mail.ustc.edu.cn>
2019-05-29 00:31:44 +0800

    update - prettier

--- src/pages/example/index.js
+++ src/pages/example/index.js
@@ -3,22 +3,26 @@ import { Button } from 'antd';
 import CButton from '../../component/Button';
 
 export default class App extends Component {
-  state = {
-    count: 1,
-    showCB: true,
-  }
+    state = {
+        count: 1,
+        showCB: true,
+    };
 
-  onClick = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
+    onClick = () => {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    };
 
-  render() {
-    return <div style={{ textAlign: 'center' }}>
-      <h1>Hello World {this.state.count}</h1>
-      <Button type='primary' onClick={this.onClick} >click me</Button>
-      <CButton>keke</CButton>
-    </div>;
-  }
+    render() {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>Hello World {this.state.count}</h1>
+                <Button type="primary" onClick={this.onClick}>
+                    click me
+                </Button>
+                <CButton>keke</CButton>
+            </div>
+        );
+    }
 }
--- src/utils/event.js
+++ src/utils/event.js
@@ -2,89 +2,82 @@
  * @Author: lixiang
  * @Date: 2019-05-26 11:22:40
  * @Last Modified by: lixiang
- * @Last Modified time: 2019-05-28 23:57:05
+ * @Last Modified time: 2019-05-29 00:31:33
  */
 
 import { isString, isFunction } from './isType';
 
 class PubSub {
-  constructor() {
-    this._cache = {};
-  }
+    constructor() {
+        this._cache = {};
+    }
 
-  on(event, ...fns) {
-    if (!isString(event)) {
-      return;
-    }
-    if (!this._cache[event]) {
-      this._cache[event] = [];
-    }
-    for (let i = 0; i < fns.length; i++) {
-      if (isFunction(fns[i])) {
-        this._cache[event].push({
-          fn: fns[i],
-          once: false,
-        });
-      }
-    }
-  }
-
-  once = (event, ...fns) => {
-    if (!isString(event)) {
-      return;
-    }
-    if (!this._cache[event]) {
-      this._cache[event] = [];
-    }
-    for (let i = 0; i < fns.length; i++) {
-      if (isFunction(fns[i])) {
-        this._cache[event].push({
-          fn: fns[i],
-          once: true,
-        });
-      }
-    }
-  }
-
-  emit(event, ...args) {
-    if (!isString(event)) {
-      return;
-    }
-    if (this._cache[event]) {
-      const events = this._cache[event];
-      for (let i = 0; i < events.length; i++) {
-        let item = events[i];
-        item.fn && item.fn(...args);
-        if (item.once) {
-          events.splice(i, 1);
-          i--;
+    on(event, ...fns) {
+        if (!isString(event)) {
+            return;
         }
-      }
-    }
-  }
-
-  removeListener = (event, fn) => {
-    if (this._cache[event]) {
-      for (let i = 0; i < this._cache[event].length; i++) {
-        if (this._cache[event][i] === fn) {
-          this._cache[event].splice(i, 1);
+        if (!this._cache[event]) {
+            this._cache[event] = [];
         }
-      }
+        for (let i = 0; i < fns.length; i++) {
+            if (isFunction(fns[i])) {
+                this._cache[event].push({
+                    fn: fns[i],
+                    once: false,
+                });
+            }
+        }
     }
-  }
 
-  removeAllListeners = (event) => {
-    if (this._cache[event]) {
-      this._cache[event] = null;
+    once = (event, ...fns) => {
+        if (!isString(event)) {
+            return;
+        }
+        if (!this._cache[event]) {
+            this._cache[event] = [];
+        }
+        for (let i = 0; i < fns.length; i++) {
+            if (isFunction(fns[i])) {
+                this._cache[event].push({
+                    fn: fns[i],
+                    once: true,
+                });
+            }
+        }
+    };
+
+    emit(event, ...args) {
+        if (!isString(event)) {
+            return;
+        }
+        if (this._cache[event]) {
+            const events = this._cache[event];
+            for (let i = 0; i < events.length; i++) {
+                let item = events[i];
+                item.fn && item.fn(...args);
+                if (item.once) {
+                    events.splice(i, 1);
+                    i--;
+                }
+            }
+        }
     }
-  }
 
+    removeListener = (event, fn) => {
+        if (this._cache[event]) {
+            for (let i = 0; i < this._cache[event].length; i++) {
+                if (this._cache[event][i] === fn) {
+                    this._cache[event].splice(i, 1);
+                }
+            }
+        }
+    };
+
+    removeAllListeners = event => {
+        if (this._cache[event]) {
+            this._cache[event] = null;
+        }
+    };
 }
-
-function test(x = "hello", { a, b }, ...args) {
-  console.log(x, a, b, args);
-}
-
-test(12, 3, 4, 5, 6, 6, 7, 8, 8);
 
 export default new PubSub();
